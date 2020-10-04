ffe19f4c31f766f896523deffd80ef9570c32a15
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2020-10-04 21:04:43 +0300

    Move to the next level added sound.

--- Assets/Game/Scripts/MoveToTheNextLevel.cs
+++ Assets/Game/Scripts/MoveToTheNextLevel.cs
@@ -10,18 +10,41 @@ public class MoveToTheNextLevel : MonoBehaviour
 
     private GameObject _spitzGameObject;
     private Collider2D _spitzCollider;
+    private SpitzController _spitzController;
+
+    public AudioSource audioSourceEndLevel;
     
     private void Start()
     {
         _spitzGameObject = GameObject.Find("Spitz");
         _spitzCollider = _spitzGameObject.GetComponent<Collider2D>();
+        _spitzController = _spitzGameObject.GetComponent<SpitzController>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (_spitzCollider == other)
         {
-            SceneManager.LoadScene(sceneName);
+            _spitzController.SetIsInteractionsAllowed(false);
+            
+            StartCoroutine(WinCoroutine(audioSourceEndLevel));
+        }
+    }
+
+    private IEnumerator WinCoroutine(AudioSource audioSource)
+    {
+        yield return AudioSourcePlayCoroutine(audioSource);
+        
+        SceneManager.LoadScene(sceneName);
+    }
+    
+    private IEnumerator AudioSourcePlayCoroutine(AudioSource audioSource)
+    {
+        audioSource.Play();
+
+        while (audioSource.isPlaying)
+        {
+            yield return null;
         }
     }
 }
