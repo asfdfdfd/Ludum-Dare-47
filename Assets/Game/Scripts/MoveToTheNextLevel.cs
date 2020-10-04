09f793c864564bbb2047955e9a3b859e42051118
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2020-10-04 19:23:06 +0300

    Move to the next level prefab.

---
+++ Assets/Game/Scripts/MoveToTheNextLevel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToTheNextLevel : MonoBehaviour
+{
+    public string sceneName;
+
+    private GameObject _spitzGameObject;
+    private Collider2D _spitzCollider;
+    
+    private void Start()
+    {
+        _spitzGameObject = GameObject.Find("Spitz");
+        _spitzCollider = _spitzGameObject.GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_spitzCollider == other)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
