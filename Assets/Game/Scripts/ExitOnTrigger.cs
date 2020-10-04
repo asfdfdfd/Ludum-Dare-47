d9055935f93993f38be09d0a3cf15a7f7948917a
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2020-10-04 20:29:12 +0300

    Sprites and first level.

---
+++ Assets/Game/Scripts/ExitOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitOnTrigger : MonoBehaviour
+{
+    private GameObject _spitzGameObject;
+    private Collider2D _spitzCollider;
+    
+    private void Start()
+    {
+        _spitzGameObject = GameObject.Find("Spitz");
+        _spitzCollider = _spitzGameObject.GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_spitzCollider == other)
+        {
+            Application.Quit();
+        }
+    }
+}
