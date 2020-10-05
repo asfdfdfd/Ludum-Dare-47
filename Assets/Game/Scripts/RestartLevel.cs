794246f384e1e8cb075e700de0d5b3312cd56730
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2020-10-06 01:41:57 +0300

    Final touches.

---
+++ Assets/Game/Scripts/RestartLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    public string levelName;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
