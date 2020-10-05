1ca0b16bdf3ebd0315a069a90f8bc990e54e19df
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2020-10-05 20:32:13 +0300

    Goose incoming.

---
+++ Assets/Game/Scripts/DelayedAudioPlay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedAudioPlay : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    public float delay;
+    
+    private void Awake()
+    {
+        _audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(DelayedPlayCoroutine());
+    }
+
+    private IEnumerator DelayedPlayCoroutine()
+    {
+        yield return new WaitForSeconds(delay);
+        
+        _audioSource.Play();
+    }
+}
